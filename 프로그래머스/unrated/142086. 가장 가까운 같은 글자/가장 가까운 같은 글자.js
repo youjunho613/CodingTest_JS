960c402931bbfb3b2a4f4a9b089bc4723dcf9931
newJuno <131266433+youjunho613@users.noreply.github.com>
2023-08-01 17:19:09 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 9.87 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,11 +1,15 @@
 function solution(s) {
-    let answer = [];
-    let staticWord = [];
-    for (let i = 0; i < s.length; i++) {
-        !staticWord.includes(s[i])
-        ? answer.push(-1)
-        : answer.push(staticWord.length - staticWord.lastIndexOf(s[i]));
-        staticWord.push(s[i]);
+  let answer = [];
+  let arr = [];
+  for (let i = 0; i < s.length; i++) {
+    const findIndex = s.indexOf(s[i]);
+    if (findIndex === i) {
+      answer.push(-1);
+      arr.push(s[i]);
+    } else {
+      arr.push(s[i]);
+      answer.push(arr.lastIndexOf(s[i], i) - arr.indexOf(s[i]));
     }
-    return answer;
+  }
+  return answer;
 }
