d1f5fde833e8f8decfb0f58b320cebeecd239435
newJuno <131266433+youjunho613@users.noreply.github.com>
2023-10-27 12:23:19 +0900

    [level 1] Title: 최소직사각형, Time: 1.92 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -2,31 +2,31 @@ function solution(sizes) {
     let bigArr = [];
     let smallArr = [];
     
-    for (let i = 0; i < sizes.length; i++) {
-        let current = sizes[i];
+//     for (let i = 0; i < sizes.length; i++) {
+//         let current = sizes[i];
         
-        if (current[0] >= current[1]) {
-            bigArr.push(current[0])
-            smallArr.push(current[1])
-        }
+//         if (current[0] >= current[1]) {
+//             bigArr.push(current[0])
+//             smallArr.push(current[1])
+//         }
         
-        if (current[1] >= current[0]) {
-            bigArr.push(current[1])
-            smallArr.push(current[0])
-        }
-    }
+//         if (current[1] >= current[0]) {
+//             bigArr.push(current[1])
+//             smallArr.push(current[0])
+//         }
+//     }
     
-//     sizes.map((wallet) => {
-//         if (wallet[0] > wallet[1]) {
-//             bigArr.push(wallet[0]);
-//             smallArr.push(wallet[1])
-//         }
+    sizes.map((wallet) => {
+        if (wallet[0] >= wallet[1]) {
+            bigArr.push(wallet[0]);
+            smallArr.push(wallet[1])
+        }
         
-//         if (wallet[1] > wallet[0]) {
-//             bigArr.push(wallet[1]);
-//             smallArr.push(wallet[0])
-//         }
-//     })
+        if (wallet[1] >= wallet[0]) {
+            bigArr.push(wallet[1]);
+            smallArr.push(wallet[0])
+        }
+    })
         
     return Math.max(...bigArr) * Math.max(...smallArr);
 }
